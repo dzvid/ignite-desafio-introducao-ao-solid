a947ca11c5386231c52846c0a28379222898be6e
David <oliveiradavid.dev@gmail.com>
2021-05-11 23:34:39 -0400

    Added usecases test fixes

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user === undefined || !user.admin) {
+      throw new Error("Not allowed to list users");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 

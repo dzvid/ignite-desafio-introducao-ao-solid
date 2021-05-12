f60896f169630c407b1a7327e36fa18dcb7604a1
David <oliveiradavid.dev@gmail.com>
2021-05-12 00:08:37 -0400

    Added controllers test fixes

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -12,7 +12,7 @@ class TurnUserAdminUseCase {
     const user = this.usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("User does not exists!");
+      throw new Error("User not found!");
     }
 
     const adminUser = this.usersRepository.turnAdmin(user);
